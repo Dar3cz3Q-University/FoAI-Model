f18650f931db98203bc2c76851c0234a923825c3
Dariusz Homa <dariuszhoma7@gmail.com>
2025-03-30 13:31:31 +0200

    feat: add publish stage

--- scripts/scripts.py
+++ scripts/scripts.py
@@ -3,12 +3,4 @@ import sys
 
 
 def run_linter():
-    sys.exit(subprocess.call(["poetry", "run", "pylint", "foaimodel/"]))
-
-
-def run_type_checker():
-    sys.exit(subprocess.call(["poetry", "run", "mypy", "foaimodel/"]))
-
-
-def run_tests():
-    sys.exit(subprocess.call(["poetry", "run", "pytest"]))
+    sys.exit(subprocess.call(["poetry", "run", "pylint", "cv_model/"]))
