71ccdae1505f29f57a2aed6f3035d4f14277b6fd
Jakub Latawiec <latawiec.jakub@outlook.com>
2025-04-08 16:46:40 +0200

    feat: model precentage predisction

--- foai_model/predict/predict.py
+++ foai_model/predict/predict.py
@@ -6,6 +6,7 @@ from huggingface_hub import hf_hub_download
 import nltk
 
 from foai_model.config import MAX_TOKEN_LENGTH
+from foai_model.logger import logger
 from foai_model.preprocessing import clean_resume
 
 nltk.download('punkt_tab')
@@ -13,11 +14,13 @@ nltk.download('punkt_tab')
 REPO_ID = "Dar3cz3Q/foai_model"
 SUBFOLDER = "checkpoint"
 
+logger.info("Loading tokenizer and model from HuggingFace hub...")
 tokenizer = AutoTokenizer.from_pretrained(REPO_ID, subfolder=SUBFOLDER)
 model = AutoModelForSequenceClassification.from_pretrained(REPO_ID, subfolder=SUBFOLDER)
 model.eval()
 device = torch.device("cpu")
 model.to(device)
+logger.info("Model loaded and moved to CPU.")
 
 encoder_path = hf_hub_download(
     repo_id=REPO_ID, filename="label_encoder.pkl", subfolder=SUBFOLDER
@@ -27,7 +30,7 @@ with open(encoder_path, "rb") as f:
     label_encoder = pickle.load(f)
 
 index_to_label = dict(enumerate(label_encoder.classes_))
-
+logger.info("Label encoder loaded: %s", index_to_label)
 
 def predict(text):
     inputs = tokenizer(
@@ -42,13 +45,20 @@ def predict(text):
 
     with torch.no_grad():
         logits = model(**inputs).logits
-        predicted_class_id = torch.argmax(logits, dim=1).item()
+        probs = torch.nn.functional.softmax(logits, dim=1).squeeze().cpu().numpy()
 
-    category = index_to_label[predicted_class_id]
+    category_distribution = {
+        label: round(float(probs[idx]) * 100, 2)
+        for idx, label in index_to_label.items()
+    }
+
+    predicted_class_id = int(torch.argmax(logits, dim=1).item())
+    predicted_label = index_to_label[predicted_class_id]
 
     return {
-        "id": predicted_class_id,
-        "category": category,
+        "predicted_id": predicted_class_id,
+        "predicted_category": predicted_label,
+        "category_distribution": category_distribution
     }
 
 
